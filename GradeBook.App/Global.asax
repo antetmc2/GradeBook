73ca776bd84834b47307d9ddb51fbeea46ba1504
antetmc2 <antetmc2@gmail.com>
2016-05-23 09:45:16 +0200

    Web app

---
+++ GradeBook.App/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GradeBook.App.MvcApplication" Language="C#" %>
---
+++ GradeBook.App/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GradeBook.App.Startup))]
+namespace GradeBook.App
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
