754372f345e227c63104bde8220163d47e01904f
antetmc2 <antetmc2@gmail.com>
2016-05-26 22:31:32 +0200

    CRUD za master

--- GradeBook.App/Controllers/SkolaController.cs
+++ GradeBook.App/Controllers/SkolaController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using GradeBook.BLL;
 
 namespace GradeBook.App.Controllers
 {
@@ -11,7 +12,55 @@ namespace GradeBook.App.Controllers
         // GET: Skola
         public ActionResult Index()
         {
-            return View();
+            return View(SkolaInfoList.Get());
+        }
+
+        public ActionResult Details(int id)
+        {
+            return View(Skola.Get(id));
+        }
+
+        public ActionResult Edit(int id)
+        {
+            Skola skola = Skola.Get(id);
+            return View(skola);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(int id, string NazivSkole, string Adresa, string Email, string MbrSkole, string OibSkole, string Telefon)
+        {
+            Skola skola = null;
+
+            skola = Skola.Get(id);
+            skola.NazivSkole = NazivSkole;
+            skola.Adresa = Adresa;
+            skola.Email = Email;
+            skola.MbrSkole = MbrSkole;
+            skola.OibSkole = OibSkole;
+            skola.Telefon = Telefon;
+            skola = skola.Save();
+
+            return RedirectToAction("Details", new { id = skola.IdSkole });
+        }
+
+        public ActionResult Create()
+        {
+            return View(Skola.New());
+        }
+
+        [HttpPost]
+        public ActionResult Create(string NazivSkole, string Adresa, string Email, string MbrSkole, string OibSkole, string Telefon)
+        {
+            Skola skola = Skola.New();
+            skola.NazivSkole = NazivSkole;
+            skola.Adresa = Adresa;
+            skola.Email = Email;
+            skola.MbrSkole = MbrSkole;
+            skola.OibSkole = OibSkole;
+            skola.Telefon = Telefon;
+            skola = skola.Save();
+
+            return RedirectToAction("Details", new { id = skola.IdSkole });
         }
     }
 }
